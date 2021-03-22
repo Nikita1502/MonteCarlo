ccb97ec4a2bc90dd08636be85be204fe265942f1
Nikita1502 <58480269+Nikita1502@users.noreply.github.com>
2021-03-23 03:11:48 +0700

    v2

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const dotsNum = document.querySelector('#dotsNum');
 const PiResult = document.querySelector('#result');
 const errorRes = document.querySelector('#errorRes');
+const loader = document.querySelector(".lds-dual-ring")
 const canvas = document.querySelector('#c1');
 const ctx = canvas.getContext('2d');
 
-const numOfDots = 1000000;
+const numOfDots = 10000000;
 
 let circleHit = 0;
 
@@ -12,7 +13,8 @@ function createDot() {
     for (let i = 0; i < numOfDots; i++) {
         const pointX = Math.random() * 2 - 1;
         const pointY = Math.random() * 2 - 1;
-        if (Math.sqrt(Math.pow(pointX, 2) + Math.pow(pointY, 2)) <= 1) {
+        const condition = Math.sqrt(Math.pow(pointX, 2) + Math.pow(pointY, 2));
+        if (condition < 1) {
             circleHit++;
             ctx.fillStyle = 'red';
             ctx.fillRect(pointX * 300 + 301, pointY * 300 + 301, 1, 1);
@@ -23,19 +25,40 @@ function createDot() {
     }
 }
 
-createDot();
+// let executionTime = 0;
+// for (let i = 0; i < 20; i++) {
+//     var time = performance.now();
+//     createDot();
+//     time = performance.now() - time;
+//
+//     console.log('Время выполнения = ', time);
+//     executionTime += time;
+// }
+// console.log('Итого ' + executionTime / 1000 + ' сек');
 
 function calculatePi() {
     let pi = (4 * circleHit) / numOfDots;
     return pi;
 }
+
 function error() {
-    let absError = Math.abs((3.1415926535897932 - calculatePi()));
-    let relError = (absError/3.1415926535897932)*100;
+    let absError = Math.abs((Math.PI - calculatePi()));
+    let relError = (absError / Math.PI) * 100;
     return relError.toFixed(6) + '%';
 }
-dotsNum.innerHTML = numOfDots;
-PiResult.innerHTML = calculatePi();
-errorRes.innerHTML = error();
+
+let promise = new Promise(function(resolve, reject) {
+    loader.style.visibility='visible';
+    createDot();
+    dotsNum.innerHTML = numOfDots;
+    PiResult.innerHTML = calculatePi();
+    errorRes.innerHTML = error();
+
+});
+promise.then(loader.style.visibility='hidden');
+
+
+
+
 
 
